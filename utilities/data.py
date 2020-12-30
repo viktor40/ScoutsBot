68c1466ba69a1c48c73005524668415eae47d257
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-12-30 09:48:14 +0100

    initial commit

--- utilities/data.py
+++ utilities/data.py
@@ -13,34 +13,19 @@ from typing_extensions import Final
 
 developer_ids: Final[list] = [234257395910443008, 561705204630683648]
 
-coordinate_channel: Final[int] = 690080048375529481
-application_channel: Final[int] = 647293235248496644
-bulletin_board_channel: Final[int] = 724371678393663569
-vote_channel_id: Final[int] = 730396648995422288
-fixed_bug_channel_id: Final[int] = 737441217754955836
+scouts_guild: Final[int] = 0
 
-hammer_guild: Final[int] = 645464470633840651
-
-upvote_emote: Final[int] = 689945780844494848
-downvote_emote: Final[int] = 689946011854307381
-votent_emote: Final[int] = 701747772461678612
+upvote_emote: Final[int] = 0
+downvote_emote: Final[int] = 0
+votent_emote: Final[int] = 0
 
 vote_emotes: Final[list] = [upvote_emote, downvote_emote, votent_emote]
 
-vote_role_id: Final[int] = 729381591197024379
-tour_giver_id: Final[int] = 702542764893536346
-never_alone_id: Final[int] = 770083452581969940
-cmp_role_id: Final[int] = 728002223887351830
-member_role_id: Final[int] = 645465607856324608
-admin_role_id: Final[int] = 649340485093031957
+role_ids: Final[dict] = {}
 
-role_ids: Final[dict] = {"tour giver": tour_giver_id,
-                         "voten'tn't": vote_role_id,
-                         "never-alone": never_alone_id}
+role_list: Final[list] = []
 
-role_list: Final[list] = ["tour giver", "voten'tn't"]
-
-hammer_bot_id: Final[int] = 724012480661028907
+scouts_bot_id: Final[int] = 0
 
 discord_letters: Final[list] = [
     "\N{REGIONAL INDICATOR SYMBOL LETTER A}",
@@ -70,11 +55,3 @@ discord_letters: Final[list] = [
     "\N{REGIONAL INDICATOR SYMBOL LETTER Y}",
     "\N{REGIONAL INDICATOR SYMBOL LETTER Z}"
     ]
-
-bug_colour_mappings: Final[dict] = {"Open": discord.Color.red(),
-                                    "In Progress": discord.Colour.orange(),
-                                    "Resolved": discord.Colour.green(),
-                                    "Reopened": discord.Colour.blue(),
-                                    "Postponed": discord.Colour.purple(),
-                                    "Closed": discord.Colour.dark_grey()
-                                    }
